fc23fb82aac2c0af6c28e97d6ca04617731ddf00
gkossareva <taccolinka@gmail.com>
2020-03-11 20:12:35 -0400

    Modified array of colors

--- src/day1/Color.java
+++ src/day1/Color.java
@@ -2,6 +2,6 @@ package day1;
 
 public class Color {
     public static void main(String[] args) {
-        String[] colors={"red","blue","green"};
+        String[] colors={"red","blue","green","yellow"};
     }
 }
